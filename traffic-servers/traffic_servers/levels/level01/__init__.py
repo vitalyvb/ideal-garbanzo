25101e18567543b777b28340d38efde5830fadac
Vitalii Bursov <vitaly@bursov.com>
2019-11-02 20:01:25 +0200

    more verbose message for level2 password

--- traffic-servers/traffic_servers/levels/level01/__init__.py
+++ traffic-servers/traffic_servers/levels/level01/__init__.py
@@ -8,7 +8,7 @@ def run(env):
     conf.iface = env.interface
     conf.ipv6_enabled = False
 
-    pkt = Ether()/IP(dst="1.2.3.4",ttl=64)/UDP(sport=53)/DNS(rd=1, qd=DNSQR(qname='password is ' + env.nextpass + ' '))
+    pkt = Ether()/IP(dst="1.2.3.4",ttl=64)/UDP(sport=53)/DNS(rd=1, qd=DNSQR(qname='Next level password is ' + env.nextpass + ' '))
 
     # no rx
     sock = L2Socket(iface=conf.iface, filter="proto 254", promisc=False)
