4d55f264c7b7cc382bb1a240501af3a99cfea1f8
Vitalii Bursov <vitaly@bursov.com>
2019-11-02 20:05:51 +0200

    level1 - print verbose message once every 10 seconds

--- traffic-servers/traffic_servers/levels/level01/__init__.py
+++ traffic-servers/traffic_servers/levels/level01/__init__.py
@@ -8,11 +8,14 @@ def run(env):
     conf.iface = env.interface
     conf.ipv6_enabled = False
 
-    pkt = Ether()/IP(dst="1.2.3.4",ttl=64)/UDP(sport=53)/DNS(rd=1, qd=DNSQR(qname='Next level password is ' + env.nextpass + ' '))
+    pkt_verbose = Ether()/IP(dst="1.2.3.4",ttl=64)/UDP(sport=53)/DNS(rd=1, qd=DNSQR(qname='Next level password is ' + env.nextpass + ' '))
+    pkt_silent = Ether()/IP(dst="1.2.3.4",ttl=64)/UDP(sport=53)/DNS(rd=1, qd=DNSQR(qname=env.nextpass))
 
     # no rx
     sock = L2Socket(iface=conf.iface, filter="proto 254", promisc=False)
+    i = 0
 
     while True:
-        sock.send(pkt)
+        sock.send(pkt_verbose if i % 10 == 0 else pkt_silent)
         time.sleep(1)
+        i += 1
